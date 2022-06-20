408485d4999b6d9157be1db3a7c419ee4f34b353
Raul Correia <raulvinicius245@gmail.com>
2022-06-20 11:49:25 -0300

    ngOnInit e ngOnChanges.

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
   constructor() { }
 
-  public teste: string = "teste";
+  @Input() public teste: string = "teste";
 
   ngOnInit(): void {
+  }
+
+  //É chamado quando há uma alteração de dados
+  ngOnChanges(): void {
+    console.log("valor alterado")
   }
 
 }
