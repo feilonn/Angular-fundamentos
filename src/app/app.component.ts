00efffa3a1ff3c02270eee5ede358e5a7c9a5ae1
Raul Correia <raulvinicius245@gmail.com>
2022-06-21 10:45:10 -0300

    Property Biding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
   //É chamado quando o component é iniciado
-  ngOnInit(): void {
-    alert("Olá");
-  }
+  // ngOnInit(): void {
+  //   alert("Olá");
+  // }
 
 }
