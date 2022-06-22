33dc03853c7ed05fcbcc0ab6a9e7a1ad74176db2
Raul Correia <raulvinicius245@gmail.com>
2022-06-22 11:26:21 -0300

    Diretiva ngFor.

--- src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -9,6 +9,26 @@ export class DiretivasEstruturaisComponent implements OnInit {
 
   public condition: boolean = true
   public conditionClick: boolean = true;
+  public cont: number = 0;
+
+  public pessoas: Array<{ nome: string, idade: number }> = [
+    {
+      nome: "Raul Correia",
+      idade:  100
+    },
+    {
+      nome: "Teste 1",
+      idade: 1
+    },
+    {
+      nome: "Teste 2",
+      idade: 1
+    },
+    {
+      nome: "Teste 3",
+      idade: 1
+    }
+  ]
 
   constructor() { }
 
@@ -20,6 +40,16 @@ export class DiretivasEstruturaisComponent implements OnInit {
 
   public onClickBool() {
     this.conditionClick = !this.conditionClick;
+  }
+
+  public pushList() {
+    this.cont++;
+    this.pessoas.push({ nome: "Novo "+this.cont, idade: this.cont + 10})
+  }
+
+  public onClickList(event: number) {
+    //Deleta o elemento da lista que foi clicado.
+    this.pessoas.splice(event, 1);
   }
 
 }
